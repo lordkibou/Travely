38cea208e2c42ebe69ee7db06cc29a53261cd997
JuanDiaz <juanben10xlr99@gmail.com>
2024-01-21 11:33:50 +0000

    Model for User Stablished and Routes as well

---
+++ backend/src/models/user.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+export type UserType = {
+    _id: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+};
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName:{ type: String, required: true },
+});
+
+//Middleware from mongodb before saving the user, change password to hashed
+userSchema.pre("save", async function (next) {
+    if(this.isModified("password")) {
+        const hashed = await bcrypt.hash(this.get("password"), 8);
+        this.set("password", hashed);
+    }
+    next();
+});
+
+const User = mongoose.model<UserType>("User", userSchema);
+
+export default User;
+
