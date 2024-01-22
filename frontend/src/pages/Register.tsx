d15316287d53845e968d29bb32c860e23cc6fe3d
JuanDiaz <juanben10xlr99@gmail.com>
2024-01-22 21:56:10 +0000

    Register Form Working Without Error Messages Showing Yet

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,27 +1,94 @@
 import { useForm } from "react-hook-form";
 
 type RegisterFormData = {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-}
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
 const Register = () => {
-  const { register } = useForm<RegisterFormData>();
+  {
+    /*register is used for the required and dealing with the fields of the form
+      we use watch to watch the password field and then compare it to the confirm password field
+      handleSubmit is used to handle the submit of the form
+    */
+  }
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
   return (
-    <form className="flex flex-col gap-5">
-          <h2 className="text-3xl font-bold">Create an Account</h2>
-          <div className="flex flex-col md:flex-row gap-5">
-              <label className="text-gray-700 text-sm font-bol flex-1">
-                  First Name
-                  <input className="border rounded w-full py-1 px-2 font-normal"></input>
-              </label>
-              <label className="text-gray-700 text-sm font-bol flex-1">
-                  Last Name
-                  <input className="border rounded w-full py-1 px-2 font-normal"></input>
-              </label>
-          </div>
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+      <h2 className="text-3xl font-bold">Create an Account</h2>
+      <div className="flex flex-col md:flex-row gap-5">
+        <label className="text-gray-700 text-sm font-bol flex-1">
+          First Name
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("firstName", { required: "This field is required" })}
+          ></input>
+        </label>
+        <label className="text-gray-700 text-sm font-bol flex-1">
+          Last Name
+          <input
+            className="border rounded w-full py-1 px-2 font-normal"
+            {...register("lastName", { required: "This field is required" })}
+          ></input>
+        </label>
+      </div>
+      <label className="text-gray-700 text-sm font-bol flex-1">
+        Email
+        <input
+          type="email"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("email", { required: "This field is required" })}
+        ></input>
+      </label>
+      <label className="text-gray-700 text-sm font-bol flex-1">
+        Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters long",
+            },
+          })}
+        ></input>
+      </label>
+      <label className="text-gray-700 text-sm font-bol flex-1">
+        Confirm Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("confirmPassword", {
+            validate: (val) => {
+              if (!val) {
+                return "This field is required";
+              } else if (watch("password") !== val) {
+                return "Passwords do not match";
+              }
+            },
+          })}
+        ></input>
+      </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white font-bold p-2 rounded text-xl hover:bg-blue-500"
+        >
+          Create an Account
+        </button>
+      </span>
     </form>
   );
 };
