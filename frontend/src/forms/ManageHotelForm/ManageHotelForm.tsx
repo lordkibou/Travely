85ea0b0ac30e9090f75fca8847dad5665f44dba2
JuanDiaz <juanben10xlr99@gmail.com>
2024-05-07 17:24:04 +0200

    Updated ManageHotelForm

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -2,14 +2,18 @@ import type { HotelFormData } from "../../types/mainTypes";
 import { FormProvider, useForm } from "react-hook-form";
 import DetailsSection from "./DetailsSection";
 import TypeSection from "./TypeSection";
+import FacilitiesSection from "./FacilitiesSection";
+import GuestsSection from "./GuestsSection";
 
 const ManageHotelForm = () => { 
     const formMethods = useForm<HotelFormData>();
     return (
         <FormProvider {...formMethods}>
-            <form>
+            <form className="flex flex-col gap-10">
                 <DetailsSection />
                 <TypeSection />
+                <FacilitiesSection />
+                <GuestsSection />
             </form>
         </FormProvider>
     );
