e7c70f6ddd8e1e5a7245cfcbb1a23eba85abafd2
JuanDiaz <juanben10xlr99@gmail.com>
2024-05-09 14:17:45 +0200

    AddHotel Done and working passing handleSave with mutation and api

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,8 +1,26 @@
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm.tsx";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client.ts";
+import { useAppContext } from "../contexts/AppContext.tsx";
 
 const AddHotel = () => {
+
+  const { showToast } = useAppContext();
+
+  const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+    onSuccess: async () => {
+      showToast({ message: "Hotel added successfully", type: "SUCCESS" });
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const handleSave = async (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
   return (
-    <ManageHotelForm/>
+    <ManageHotelForm onSave={handleSave} isLoading = {isLoading} />
   );
 };
 export default AddHotel;
